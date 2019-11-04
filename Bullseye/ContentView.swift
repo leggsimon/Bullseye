dc339611e1b726ce6c01b454eec4d0cd5e65d955
Simon Legg <leggsimon@gmail.com>
2019-11-04 21:28:52 +0100

    add popup

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+
+	@State var alertIsVisible: Bool = false
+
+	var body: some View {
+		VStack {
+			Text("My first Swift UI App")
+				.fontWeight(.semibold)
+			Button(action: {
+				print("Button pressed!!!")
+				self.alertIsVisible = true
+			}) {
+				Text("Hit Me!")
+			}
+			.alert(isPresented: $alertIsVisible){
+				() -> Alert in
+				return Alert(
+					title: Text("Hello There"),
+					message: Text("This is my first pop up!"),
+					dismissButton: .default(Text("Awesome"))
+				)
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
